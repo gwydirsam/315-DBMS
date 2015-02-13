6340fe9cf06c627c5e951b6348c9551a95e25ff9
Sam Gwydir <gwydir8@gmail.com>
2015-02-13 07:34:43 -0600

    Added my simple main

---
+++ engine/src/main.cc
@@ -0,0 +1,12 @@
+// Rabia Gunonu, Sam Gwydir, Nicolas Juarez, Daniel Koon
+// CSCE 315-503
+// main.cc
+
+#include <iostream>
+#include "dbms.h"
+
+int main(int argc, char *argv[]) {
+  std::cout << "This is the Database Engine";
+
+  return 0;
+}
