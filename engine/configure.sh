e06d81e0b86b41c8f8957a7a6f50e9c1484d927a
Sam Gwydir <gwydir8@gmail.com>
2015-02-14 11:49:30 -0600

    Updated messages

--- engine/configure.sh
+++ engine/configure.sh
@@ -3,8 +3,7 @@ echo "Removing old build directory"
 rm -rf build;
 echo "Creating build directory..."
 mkdir build;
-echo "cd to build directory..."
 cd build;
 echo "Running cmake"
 CC="/opt/csw/bin/gcc" CXX="/opt/csw/bin/g++" cmake ..
-echo "Now run make. If successful binaries will be in src/"
+echo "Now run make in build/. If successful binaries will be in build/src/"
