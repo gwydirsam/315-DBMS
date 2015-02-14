c954bcea2d36c047ed61062cb3ae64e8d3868380
Sam Gwydir <gwydir8@gmail.com>
2015-02-14 12:54:44 -0600

    configure now detects os

--- engine/configure.sh
+++ engine/configure.sh
@@ -1,9 +1,20 @@
 #!/bin/bash
+UNAME=`uname`
 echo "Removing old build directory"
 rm -rf build;
 echo "Creating build directory..."
 mkdir build;
 cd build;
 echo "Running cmake"
-CC="/opt/csw/bin/gcc -fdiagnostics-color=auto" CXX="/opt/csw/bin/g++ -fdiagnostics-color=auto" cmake ..
+#if OS X
+if [ "$UNAME" = "Darwin" ]
+then
+    CC="/usr/local/opt/llvm/bin/clang" CXX="/usr/local/opt/llvm/bin/clang++" cmake ..
+# if Solaris
+elif [ "$UNAME" = "SunOS" ]
+then
+    CC="/opt/csw/bin/gcc -fdiagnostics-color=auto" CXX="/opt/csw/bin/g++ -fdiagnostics-color=auto" cmake ..
+else
+    cmake ..
+fi
 echo "Now run make in build/. If successful binaries will be in build/src/"
