8a54460c4246e55adda5b2ac75accdfc4600c2e7
Sam Gwydir <gwydir8@gmail.com>
2015-02-14 12:00:12 -0600

    Added color to g++ output

--- engine/configure.sh
+++ engine/configure.sh
@@ -5,5 +5,5 @@ echo "Creating build directory..."
 mkdir build;
 cd build;
 echo "Running cmake"
-CC="/opt/csw/bin/gcc" CXX="/opt/csw/bin/g++" cmake ..
+CC="/opt/csw/bin/gcc -fdiagnostics-color=auto" CXX="/opt/csw/bin/g++ -fdiagnostics-color=auto" cmake ..
 echo "Now run make in build/. If successful binaries will be in build/src/"
