ab455aec31db3a346c0f59ac5f9a876aae5d500c
Sam Gwydir <gwydir8@gmail.com>
2015-02-14 18:30:51 -0600

    Main creates a new Table

--- engine/src/main.cc
+++ engine/src/main.cc
@@ -4,9 +4,15 @@
 
 #include <iostream>
 #include "engine.h"
+#include "column.h"
 
 int main(int argc, char *argv[]) {
-  std::cout << "This is the Database Engine";
+  std::cout << "This is the Database Engine" << std::endl;
+  Engine db;
+  // Column<std::string> column("ID");
+  std::vector<Column<std::string> > columns;
+  columns.push_back(Column<std::string>("ID"));
+  db.createNewTable("Test", columns);
 
   return 0;
 }
