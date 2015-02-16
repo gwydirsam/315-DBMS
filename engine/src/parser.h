5be075839b3aec0783f839f4af9dcda75ff52c32
Sam Gwydir <gwydir8@gmail.com>
2015-02-16 16:23:19 -0600

    Stubbed out parser. Added to cmake

---
+++ engine/src/parser.h
@@ -0,0 +1,20 @@
+// CSCE 315-503
+// parser.h
+
+#ifndef PARSER_H_
+#define PARSER_H_
+
+class Parser {
+ public:
+  // Constructors
+  // Default Constructor
+  Parser(){};
+
+  // Destructors
+  // Default Destructor
+  ~Parser(){};
+
+ private:
+};
+
+#endif  // PARSER_H_
