d42d045480302927aee17fcaf1207b99f2ff7104
Sam Gwydir <gwydir8@gmail.com>
2015-02-17 15:37:55 -0600

    Added tokenizer

--- engine/lib/parser.h
+++ engine/lib/parser.h
@@ -4,6 +4,9 @@
 #ifndef PARSER_H_
 #define PARSER_H_
 
+#include <string>
+#include <vector>
+
 class Parser {
  public:
   // Constructors
@@ -16,10 +19,16 @@ class Parser {
 
   // Receives command and returns a string of that command.
   std::string receive_command(std::string command);
+
   // Translate a command to Engine command string.
   std::string translate_command(std::string command);
+
   // Sends the engine command to be executed or excutes it here.
   void exec_engine_command(std::string command);
+
+  // Breaks a string into a vector of strings where each string in the vector is
+  // one word (space seperated) from the original strings
+  std::vector<std::string> tokenize(std::string query);
 
  private:
 };
