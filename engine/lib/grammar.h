3e3366c4198c863ff5134175f350489e63cab904
Sam Gwydir <gwydir8@gmail.com>
2015-03-01 13:35:38 -0600

    Using recursive wrappers for program et al

--- engine/lib/grammar.h
+++ engine/lib/grammar.h
@@ -15,44 +15,47 @@
 
 #include <boost/algorithm/string.hpp>
 
+#include <boost/variant.hpp>
+#include <boost/variant/recursive_variant.hpp>
+#include <boost/fusion/adapted/std_pair.hpp>
+#include <boost/fusion/include/std_pair.hpp>
+
 #include <boost/fusion/adapted.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/qi_as.hpp>
 
+#include <boost/spirit/include/phoenix_core.hpp>
+#include <boost/spirit/include/phoenix_fusion.hpp>
+#include <boost/spirit/include/phoenix_stl.hpp>
 #include <boost/spirit/include/phoenix_operator.hpp>
 #include <boost/spirit/include/phoenix_object.hpp>
 
 #include "grammar_objects.h"
-
-BOOST_FUSION_ADAPT_STRUCT(Expression,
-                          (std::string, relation_name)(std::vector<std::string>,
-                                                       expression))
-
-// BOOST_FUSION_ADAPT_STRUCT(Query,
-//                           (std::string, relation_name)
-//                           (std::vector<std::string>, expression))
-BOOST_FUSION_ADAPT_STRUCT(Query,
-                          (std::string, relation_name)(Expression, expression))
-
-BOOST_FUSION_ADAPT_STRUCT(Argument,
-                          (std::string, relation_name)(std::vector<std::string>,
-                                                       entries))
-
-// BOOST_FUSION_ADAPT_STRUCT(Command,
-//                           (std::string, command)
-//                           (std::vector<std::string>, argument))
-BOOST_FUSION_ADAPT_STRUCT(Command, (std::string, command)(Argument, argument))
-
-BOOST_FUSION_ADAPT_STRUCT(Program, (Query, query)(Command, command))
 
 template <typename It = std::string::const_iterator,
           typename Skipper = boost::spirit::qi::space_type>
 class Grammar : public boost::spirit::qi::grammar<It, Program(), Skipper> {
  public:
   Grammar() : Grammar::base_type(program) {
-    using namespace boost::spirit::qi;
     using boost::spirit::qi::as;
     using boost::spirit::qi::as_string;
+    using boost::spirit::qi::lit;
+    using boost::spirit::qi::lexeme;
+    using boost::spirit::qi::hold;
+    using boost::spirit::qi::no_case;
+    using boost::spirit::qi::attr;
+    using boost::spirit::qi::_1;
+    using boost::spirit::qi::_2;
+    using boost::spirit::qi::_3;
+
+    using boost::spirit::ascii::char_;
+    using boost::spirit::ascii::string;
+    using boost::spirit::ascii::alnum;
+    using boost::spirit::ascii::digit;
+    using namespace boost::spirit::qi::labels;
+
+    using boost::phoenix::at_c;
+    using boost::phoenix::push_back;
 
     // open, close, write and exit
     io_cmd = hold[no_case[string("open")]] | hold[no_case[string("close")]] |
@@ -120,7 +123,24 @@ class Grammar : public boost::spirit::qi::grammar<It, Program(), Skipper> {
     comparison = hold[(operand >> op >> operand)] |
                  (string("(") >> condition >> string(")"));
 
-    // op := == | != | < | > | <= | >=
+    // // condition := conjunction { || conjunction }
+    // conditions = (conjunctions >> *("||" >> conjunctions))[_val = boost::phoenix::construct<Condition>("OR", _1, _2)];
+    // // conjunction := comparison { && comparison }
+    // conjunctions = (comparisons >> *("&&" >> comparisons))[_val = boost::phoenix::construct<Condition>("AND", _1, _2)];
+    // // comparison := operand op operand | ( condition )
+    // comparisons = (hold[(operand >> op >> operand)]) |
+    //              ("(" >> conditions >> ")");
+
+    // condition = as<std::vector<std::string>>()[conditions];
+    // conditions = subcondition >> +(op >> subcondition);
+    // subcondition = hold["(" >> conditions >> ")"] | operand;
+
+    // // op := == | != | < | > | <= | >=
+    // op = hold[string("||")] | hold[string("&&")] | hold[string("==")] | hold[string("!=")] | hold[string("<")] |
+    //      hold[string(">")] | hold[string("<=")] | string(">=");
+
+    // old op
+    // // op := == | != | < | > | <= | >=
     op = hold[string("==")] | hold[string("!=")] | hold[string("<")] |
          hold[string(">")] | hold[string("<=")] | string(">=");
 
@@ -131,7 +151,19 @@ class Grammar : public boost::spirit::qi::grammar<It, Program(), Skipper> {
     attribute_name = identifier.alias();
 
     // literal := intentionally left unspecified (strings, numbers, etc.)
-    literal = lexeme[as_string[-char_("\"") >> +alnum >> -char_("\"")]];
+    literal = lexeme[as_string[-char_('"') >> +alnum >> -char_('"')]];
+
+    // type := VARCHAR ( integer ) | INTEGER
+    type = hold[(no_case[string("varchar")] >> string("(") >> +digit >>
+                 string(")"))] |
+           no_case[string("integer")];
+
+    // identifier := alpha { ( alpha | digit ) }
+    identifier = lexeme[as_string[!lit('"') >> *(char_("a-zA-Z_")) >>
+                                  *(char_("0-9a-zA-Z_"))]];
+
+    // relation-name := identifier
+    relation_name = identifier.alias();
 
     // attribute-list := attribute-name { , attribute-name }
     attribute_list = (attribute_name >> *(',' >> attribute_name)) - ')';
@@ -140,49 +172,58 @@ class Grammar : public boost::spirit::qi::grammar<It, Program(), Skipper> {
     typed_attribute_list =
         (attribute_name >> type >> *(',' >> attribute_name >> type)) - ')';
 
-    // type := VARCHAR ( integer ) | INTEGER
-    type = hold[(no_case[string("varchar")] >> string("(") >> +digit >>
-                 string(")"))] |
-           no_case[string("integer")];
-
     // atomic-expr := relation-name | ( expr )
     atomic_expression =
-        hold[(string("(") >> expression >> string(")"))] | relation_name;
+        hold[("(" >> expression >> ")")] | relation_name;
 
+    atomic_expressions =
+        hold[("(" >> expressions >> ")")] | relation_name;
+
+    // // selection := select ( condition ) atomic-expr
+    // selection = no_case[string("select")] >> string("(") >> condition >>
+    //             string(")") >> atomic_expression;
     // selection := select ( condition ) atomic-expr
-    selection = no_case[string("select")] >> string("(") >> condition >>
-                string(")") >> atomic_expression;
+    selection =
+        (no_case[string("select")] >> ("(" >> condition >> ")") >>
+         atomic_expressions)[_val =
+                            boost::phoenix::construct<Expression>(_1, _2, _3)];
 
     // projection := project ( attribute-list ) atomic-expr
-    projection = no_case[string("project")] >> string("(") >> attribute_list >>
-                 string(")") >> atomic_expression;
+    projection = (no_case[string("project")] >> ("(" >> attribute_list >>
+                                                 ")") >> atomic_expressions)[_val = boost::phoenix::construct<Expression>(_1, _2, _3)];
     // renaming := rename ( attribute-list ) atomic-expr
-    renaming = no_case[string("rename")] >> string("(") >> attribute_list >>
-               string(")") >> atomic_expression;
+    renaming = (no_case[string("rename")] >> ("(" >> attribute_list >>
+                                              ")") >> atomic_expressions)[_val = boost::phoenix::construct<Expression>(_1,_2,_3)];
 
+    // // setunion := atomic-expr + atomic-expr
+    // setunion = attr("+") >> atomic_expression >> '+' >> atomic_expression;
+    // // difference := atomic-expr - atomic-expr
+    // difference = attr("-") >> atomic_expression >> '-' >> atomic_expression;
+    // // product := atomic-expr * atomic-expr
+    // product = attr("*") >> atomic_expression >> '*' >> atomic_expression;
     // setunion := atomic-expr + atomic-expr
-    setunion = attr("+") >> atomic_expression >> '+' >> atomic_expression;
+    setunion = (atomic_expressions >> '+' >> atomic_expressions)[_val = boost::phoenix::construct<Expression>("union",_1,_2)];
     // difference := atomic-expr - atomic-expr
-    difference = attr("-") >> atomic_expression >> '-' >> atomic_expression;
+    difference = (atomic_expressions >> '-' >> atomic_expressions)[_val = boost::phoenix::construct<Expression>("difference",_1,_2)];
     // product := atomic-expr * atomic-expr
-    product = attr("*") >> atomic_expression >> '*' >> atomic_expression;
+    product = (atomic_expressions >> '*' >> atomic_expressions)[_val = boost::phoenix::construct<Expression>("product",_1,_2)];
 
     // expr := atomic-expr | selection | projection | renaming | union |
     // difference // | product
+    // expression =
+    //     (hold[selection] | hold[projection] | hold[renaming] | hold[setunion] |
+    //      hold[difference] | hold[product] | atomic_expression) -
+    //     ';';
     expression =
-        (hold[selection] | hold[projection] | hold[renaming] | hold[setunion] |
-         hold[difference] | hold[product] | atomic_expression) -
+        (atomic_expression) -
         ';';
 
-    // identifier := alpha { ( alpha | digit ) }
-    identifier = lexeme[as_string[!char_("\"") >> *(char_("a-zA-Z_")) >>
-                                  *(char_("0-9a-zA-Z_"))]];
-
-    // relation-name := identifier
-    relation_name = identifier.alias();
+    expressions = (hold[selection] | hold[projection] | hold[renaming] | hold[setunion] |
+         hold[difference] | hold[product] | atomic_expressions) - ';';
 
     // query := relation-name <- expr ;
-    query = relation_name >> "<-" >> expression >> ';';
+    // query = relation_name >> "<-" >> expression >> ';';
+    query = relation_name >> "<-" >> (hold[expressions]|expression) >> ';';
 
     // program := { ( query | command ) }
     program = hold[query] | command;
@@ -192,7 +233,9 @@ class Grammar : public boost::spirit::qi::grammar<It, Program(), Skipper> {
     BOOST_SPIRIT_DEBUG_NODE(query);
     BOOST_SPIRIT_DEBUG_NODE(relation_name);
     BOOST_SPIRIT_DEBUG_NODE(expression);
+    BOOST_SPIRIT_DEBUG_NODE(expressions);
     BOOST_SPIRIT_DEBUG_NODE(atomic_expression);
+    BOOST_SPIRIT_DEBUG_NODE(atomic_expressions);
     BOOST_SPIRIT_DEBUG_NODE(selection);
     BOOST_SPIRIT_DEBUG_NODE(projection);
     BOOST_SPIRIT_DEBUG_NODE(renaming);
@@ -203,6 +246,9 @@ class Grammar : public boost::spirit::qi::grammar<It, Program(), Skipper> {
     BOOST_SPIRIT_DEBUG_NODE(condition);
     BOOST_SPIRIT_DEBUG_NODE(conjunction);
     BOOST_SPIRIT_DEBUG_NODE(comparison);
+    // BOOST_SPIRIT_DEBUG_NODE(conditions);
+    // BOOST_SPIRIT_DEBUG_NODE(conjunctions);
+    // BOOST_SPIRIT_DEBUG_NODE(comparisons);
     BOOST_SPIRIT_DEBUG_NODE(op);
     BOOST_SPIRIT_DEBUG_NODE(operand);
     BOOST_SPIRIT_DEBUG_NODE(attribute_name);
@@ -235,16 +281,31 @@ class Grammar : public boost::spirit::qi::grammar<It, Program(), Skipper> {
 
   boost::spirit::qi::rule<It, Query(), Skipper> query;
   boost::spirit::qi::rule<It, std::string(), Skipper> relation_name;
+  // boost::spirit::qi::rule<It, std::vector<std::string>(), Skipper> expression;
   boost::spirit::qi::rule<It, std::vector<std::string>(), Skipper> expression;
+  boost::spirit::qi::rule<It, Expression(), Skipper> expressions;
 
   boost::spirit::qi::rule<It, std::vector<std::string>(), Skipper>
       atomic_expression;
 
-  boost::spirit::qi::rule<It, std::vector<std::string>(), Skipper> selection,
+  boost::spirit::qi::rule<It, SubExpression(), Skipper>
+      atomic_expressions;
+
+  // boost::spirit::qi::rule<It, std::vector<std::string>(), Skipper> selection,
+  //     projection, renaming, setunion, difference, product;
+  boost::spirit::qi::rule<It, Expression(), Skipper> selection,
       projection, renaming, setunion, difference, product;
 
   boost::spirit::qi::rule<It, std::vector<std::string>(), Skipper> condition,
       conjunction, comparison;
+
+  // boost::spirit::qi::rule<It, Condition(), Skipper> conditions;
+  // boost::spirit::qi::rule<It, SubCondition(), Skipper> conjunctions,
+  //     comparisons;
+
+  // boost::spirit::qi::rule<It, Condition(), Skipper> conditions;
+
+  // boost::spirit::qi::rule<It, SubCondition(), Skipper> subcondition;
 
   boost::spirit::qi::rule<It, std::vector<std::string>(), Skipper>
       attribute_list, typed_attribute_list;
